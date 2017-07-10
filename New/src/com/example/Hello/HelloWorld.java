1adef1cdcfe29a66055d5ce9148c5e2a5c46e46e
4erep <kon.rudnev@gmail.com>
2017-07-10 20:45:15 +0700

    Вывод ФИО и пароля

---
+++ New/src/com/example/Hello/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.Hello;
+
+/**
+ * Created by nsk4erep on 10.07.2017.
+ */
+public class HelloWorld {
+    public static void main (String[] args) {
+        String name = "Ivan";
+        String surname = "Petrov";
+        int password = 12345;
+        System.out.println("FIO:"+name+" "+surname);
+        System.out.println("Password:"+password);
+    }
+}
